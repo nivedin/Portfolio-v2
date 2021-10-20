c0cb0ac1f9f7a60debdb57c43e09a8650250b5ef
Nivedin <nivedinp@gmail.com>
2021-10-20 22:54:58 +0530

    Head tag SEO added--bug

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Html, Main, NextScript } from "next/document";
-import HeadTag from "../components/HeadTag";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Header from "../components/Header";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +10,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <HeadTag />
+        <Head>
+          <title>Nivedin</title>
+          <meta
+            name="description"
+            content="✌️ Hey, I'm Nivedin P Suryan a Front End Developer working remotely for Digital Vault having 1.5 years of experience in web development,
+              mostly with Front-end but working on backend as well."
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
